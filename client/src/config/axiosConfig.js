5def4fa15ec949d63e9e85b91d9c718df9ddee0f
codeby-sagar <sagar.gound.work@gmail.com>
2026-04-09 08:44:48 +0530

    Update Axios base URL to production endpoint

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://social-media-eyt0.onrender.com/api",
   withCredentials: true
 });
 
