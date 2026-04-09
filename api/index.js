10fb393c3c4e2848a8d9fa7984f1939a3c492b17
Sagar Gound <sagargound0410@gmail.com>
2026-04-09 10:36:50 +0530

    Refactor code structure for improved readability and maintainability

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // middlewares
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3000,",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
