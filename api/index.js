ccbfbb72651c6805d997924224e2cd37d2cb31b2
Sagar Gound <sagargound0410@gmail.com>
2026-04-11 19:15:13 +0530

    fix: Add CORS preflight handling for all routes

--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet({
